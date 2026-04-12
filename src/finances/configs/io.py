f5780b9b90eeff8b4a4485ac4a526aad33718bd6
Juan Quintana <juan.armaguedon@icloud.com>
2026-04-12 18:53:19 +0200

    feat(data.Alphavantage): it was added new source for symbols

--- src/finances/configs/io.py
+++ src/finances/configs/io.py
@@ -2,7 +2,7 @@
 # @Author: Juan Quintana
 # @Date:   2026-03-28 23:34:49
 # @Last Modified by:   Juan Quintana
-# @Last Modified time: 2026-04-11 21:46:21
+# @Last Modified time: 2026-04-12 14:30:29
 
 import os
 
@@ -13,6 +13,8 @@ import finances.configs.env as penv
 # historical / raw
 folder_raw = os.path.join(penv.FOLDER_DATA, "historical", "raw")
 folder_raw_FinanceDatabase_symbols = os.path.join(folder_raw, "FinanceDatabase", "symbols")
+folder_raw_AlphaVantage_symbols = os.path.join(folder_raw, "AlphaVantage", "symbols")
+
 # historical / processed
 folder_processed = os.path.join(penv.FOLDER_DATA, "historical", "processed")
 folder_symbols = os.path.join(folder_processed, "symbols")
@@ -20,14 +22,18 @@ folder_symbols = os.path.join(folder_processed, "symbols")
 """ data paths symbols """
 
 # historical / raw / symbols
-path_FinanceDatabase_symbols_etfs = os.path.join(
+path_raw_FinanceDatabase_symbols_etfs = os.path.join(
     folder_raw_FinanceDatabase_symbols, "symbols_FinanceDatabase_etfs.csv"
 )
-path_FinanceDatabase_symbols_indices = os.path.join(
+path_raw_FinanceDatabase_symbols_indices = os.path.join(
     folder_raw_FinanceDatabase_symbols, "symbols_FinanceDatabase_indices.csv"
 )
-path_FinanceDatabase_symbols_funds = os.path.join(
+path_raw_FinanceDatabase_symbols_funds = os.path.join(
     folder_raw_FinanceDatabase_symbols, "symbols_FinanceDatabase_funds.csv"
 )
+path_raw_AlphaVantage_symbols = os.path.join(
+    folder_raw_AlphaVantage_symbols, "symbols_AlphaVantage.csv"
+)
 # historical / processed / symbols
-path_symbols_FinanceDatabase = os.path.join(folder_symbols, "symbols_FinanceDatabase.csv")
+path_processed_FinanceDatabase_symbols = os.path.join(folder_symbols, "symbols_FinanceDatabase.csv")
+path_processed_AlphaVantage_symbols = os.path.join(folder_symbols, "symbols_AlphaVantage.csv")
--- tasks/data.py
+++ tasks/data.py
@@ -2,8 +2,10 @@
 # @Author: Juan Quintana
 # @Date:   2026-04-11 16:19:27
 # @Last Modified by:   Juan Quintana
-# @Last Modified time: 2026-04-11 22:02:04
+# @Last Modified time: 2026-04-12 18:50:18
 
+from finances.data.AlphaVantage.cleaning.symbols import raw_to_processed as raw_to_processed_av
+from finances.data.AlphaVantage.downloader.symbols import save as save_av
 from finances.data.FinanceDatabase.cleaning.symbols import raw_to_processed as raw_to_processed_fd
 from finances.data.FinanceDatabase.downloader.symbols import save as save_fd
 from invoke.context import Context
@@ -14,10 +16,13 @@ from invoke.tasks import task
 def symbols(ctx: Context) -> None:
     """Collect symbols data
 
-    In this process will be included different sources and
-    process:
-    - downloading
-    - cleaning
+    In this process will be included different sources:
+        - AlphaVantage
+        - FinanceDatabase
+    For each source, the following steps will be executed:
+    and processes:
+        - downloading
+        - cleaning
     """
 
     ## FinanceDatabase
@@ -26,3 +31,10 @@ def symbols(ctx: Context) -> None:
     save_fd()
     # raw to processed from FinanceDatabase
     raw_to_processed_fd()
+
+    ## AlphaVantage
+
+    # save symbols from AlphaVantage
+    save_av()
+    # raw to processed from AlphaVantage
+    raw_to_processed_av()
